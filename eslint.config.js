d4527f35ac6da77428755056eaf611083708137d
Ankit Mittal <191123870+AnkitnMittal@users.noreply.github.com>
2026-02-13 18:05:18 +0000

    fix: improve existing starter code
    - move ignores inside eslint.config.js
    - update webpack.common.js to support (< Node 18)

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,10 @@ import globals from 'globals';
 import prettier from 'eslint-config-prettier';
 
 export default [
+    {
+        ignores: ['dist', 'node_modules']
+    },
+
     js.configs.recommended,
 
     {
--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,15 @@
 import path from "node:path";
+import { fileURLToPath } from "node:url";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default {
     entry: "./src/index.js",
     output: {
         filename: "main.js",
-        path: path.resolve(import.meta.dirname, "dist"),
+        path: path.resolve(__dirname, "dist"),
         clean: true,
     },
     plugins: [
