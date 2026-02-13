6670bda607455024632bdcc8d43134e4afa2c77b
Ankit Mittal <191123870+AnkitnMittal@users.noreply.github.com>
2026-02-13 16:55:52 +0000

    Webpack Configuration and Initial Setup
    - Check package.json for dependencies and scripts
    - Webpack configuration files for dev, build & deploy
    - Starter src/ folder
    - 'Node' template .gitignore file

---
+++ webpack.prod.js
@@ -0,0 +1,10 @@
+import { merge } from "webpack-merge";
+import common from "./webpack.common.js";
+import path from "node:path";
+
+export default merge(common, {
+    mode: "production",
+    output: {
+        publicPath: "auto",
+    },
+});
